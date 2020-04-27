982eb65af5641de5f78c60e54c287bf4a0690832
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-04-27 19:30:03 +0530

    Update Repeat and Missing Number Array.cpp

--- Arrays/Array Math/Repeat and Missing Number Array.cpp
+++ Arrays/Array Math/Repeat and Missing Number Array.cpp
@@ -1,25 +1,43 @@
 vector<int> Solution::repeatedNumber(const vector<int> &A) {
-    long long soa=0,s=0,poa=0,p=0;
-    long long n=A.size();
-    for(long long i=0;i<n;i++){
-        soa+=A[i];
-        poa+=A[i]*A[i];
-        p+=(i+1)*(i+1);
-        s+=(i+1);
+    int n = A.size();
+    long xo = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        xo = xo ^ A[i-1] ^ i;
     }
-    //cout<<s<<" "<<soa<<" "<<p<<" "<<poa<<endl;
-    long long x=p-poa;
-    x/=(s-soa);
-    x-=s;
-    x+=soa;
-    x/=2;
-    long long y=s-soa+x;
-    //soa-x+y=s;
-    //poa*y/x=p;
-    vector<int>arr;
-    x=int(x);
-    y=int(y);
-    arr.push_back(x);
-    arr.push_back(y);
-    return arr;
+    vector<int>ans;
+    int temp = xo ^ (xo&(xo-1));
+    int cnt1 = 0, cnt2 = 0, var1 = 0, var2 = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(temp & (i+1))
+        {
+            var1 ^= (i+1);
+            cnt1++;
+        }
+        else
+        {
+            var2 ^= (i+1);
+        }
+        if(temp & A[i])
+        {
+            var1 ^= A[i];
+            cnt2++;
+        }
+        else
+        {
+            var2 ^= A[i];
+        }
+    }
+    if(cnt1 > cnt2)
+    {
+        ans.push_back(var2);
+        ans.push_back(var1);
+    }
+    else
+    {
+        ans.push_back(var1);
+        ans.push_back(var2);
+    }
+    return ans;
 }
